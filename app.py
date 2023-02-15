23883c69dcd738ba34828d3bef810974ac59261b
JoaquinMorais <joaquinmorais2005@gmail.com>
2023-02-15 16:42:41 -0300

    railway

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from routes.home import Home
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://bdi:pepe1234@localhost/CatolicaDb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:c3koV2O5uaUGyYFIskBa@containers-us-west-78.railway.app:6912/railway"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
